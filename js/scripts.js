1392cb841914f349e403840f775f48c6910f408e
HenorAbdyli <142497426+HenorAbdyli@users.noreply.github.com>
2023-08-18 18:58:57 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// Document Ready
+$(document).ready(function () {
+
+
+});
+
+
+$(window).on('load', function () {
+    $('#loader').hide();
+}) 
+
+$(window).on('load', function () {
+    $('#loader1').hide();
+}) 
+
+$(window).on('load', function () {
+    $('#loader2').hide();
+}) 
+
+$(window).on('load', function () {
+    $('#loader3').hide();
+}) 
